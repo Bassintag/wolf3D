32a02cafe4380717f874ed89a288b02fe0a1544d
Antoine <antoine.stempfer@epitech.eu>
2017-01-15 20:21:18 +0100

    Player now dies

--- src/update_player.c
+++ src/update_player.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.stempfer@epitech.net>
 ** 
 ** Started on  Wed Dec 21 12:58:25 2016 Antoine Stempfer
-** Last update Wed Dec 21 12:59:21 2016 Antoine Stempfer
+** Last update Sun Jan 15 20:20:35 2017 Antoine Stempfer
 */
 
 #include "wolf.h"
@@ -14,4 +14,6 @@ void	update_player(t_player *player, t_map *map)
 {
   if (player->hit_timer > 0)
     player->hit_timer -= map->app->delta;
+  if (player->health <= 0)
+    sfRenderWindow_close(map->app->window);
 }
